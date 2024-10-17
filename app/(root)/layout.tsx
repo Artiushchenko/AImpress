98fc45983b93592059ede1f8e1f8551417870ac5
Artiushchenko <Artyushchenko2018@gmail.com>
2024-10-17 15:15:15 +0300

    Clerk authentication and authorization have been implemented

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,9 @@ import React from "react";
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <main className="root">
+            {/* Sidebar */}
+            {/* Mobile Navigation */}
+            
             <div className="root-container">
                 <div className="wrapper">
                     {children}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 import { cn } from "@/lib/utils";
 
 import "./globals.css";
@@ -22,10 +24,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider 
+      afterSignOutUrl='/'
+      appearance={{
+        variables: { colorPrimary: '#624cf5' }
+      }}
+    >
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
