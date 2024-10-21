1a25ba4ce6d2df79a3dfef5d34839c8993c0ce6c
Artiushchenko <Artyushchenko2018@gmail.com>
2024-10-22 00:23:51 +0300

    Image server actions were implemented

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -78,7 +78,7 @@ export async function updateCredits(userId: string, creditFee: number) {
 
 		const updatedUserCredits = await User.findOneAndUpdate(
 			{ _id: userId },
-			{ $inc: { credits: creditFee } },
+			{ $inc: { creditBalance: creditFee } },
 			{ new: true }
 		)
 
